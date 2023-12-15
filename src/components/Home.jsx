df82462d98facdf7f165ea1a963f5df0db33f0d3
Ian <69704125+ian-cunha@users.noreply.github.com>
2023-12-15 12:04:22 -0300

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,12 @@ import site from '../assets/base1.png'
 
 function Home() {
     return (
-        <div className='base1'>
+        <div className='base1 d-flex flex-column justify-content-center align-items-center"'>
             <div className="d-flex flex-sm-row flex-column justify-content-center align-items-center">
-                <div className="img1 col-md-5">
+                <div className="img1">
                     <img className='siteImg' src={site} />
                 </div>
-                <div className="text1 col-md-5">
+                <div className="text1">
                     <h1 className="text-light fs-3 pb-2 pt-2">Monte Seu Site Personalizado: Liberdade e Criatividade ao Seu Alcance!</h1>
                     <h2 className="text-light fs-6">Criado para ajudar você a priorizar negócios, acompanhar seu desempenho e não perder oportunidades.</h2>
 
